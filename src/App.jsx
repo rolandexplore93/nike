68be32bade07e8d4869bf49b645096950d2cca0b
rolandexplore93 <roland2rule@gmail.com>
2023-08-23 11:29:43 +0100

    Implement Shoes collections display on Hero UI

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Navbar } from './components';
 const App = () => (
   <main className="relative">
     <Navbar />
-    <section className="xl:padding-1 wide:padding-r padding-b">
+    <section className="xl:padding-l wide:padding-r padding-b">
       <Hero />
     </section>
     <section className="padding">
--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -7,7 +7,7 @@ const ShoeCard = ({ imgURL, changeBigShoeImage, bigShowImage }) => {
 
   return (
     <div className={`border-2 rounded-xl 
-        ${bigShowImage === imgURL ? 'border-coral-red' : 'border-transparent'}
+        ${bigShowImage === imgURL.bigShoe ? 'border-coral-red' : 'border-transparent'}
         cursor-pointer max-sm:flex-1
     `}
     onClick={handleShoeClick}
